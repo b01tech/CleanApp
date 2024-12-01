8be70e7724b42eeacf135c7b289db7000592a829
b01tech <bruno01.tech@gmail.com>
2024-12-01 17:52:49 -0300

    feat:add base entity and user

---
+++ src/Backend/CleanApp.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanApp.Domain.Entities;
+public class BaseEntity
+{
+    public long Id { get; set; }
+    public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; } = true;
+}
